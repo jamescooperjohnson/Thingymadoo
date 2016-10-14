d5197a61e2a46fcaed5a6d0b36743679ddf972e0
CooperJohnon <CooperJohnon@192.168.1.16>
2016-10-14 12:16:18 -0600

    final

--- Gmae/src/Ask.java
+++ Gmae/src/Ask.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Ask
 	{
-
+	static Integer userScore=0;
+	static Integer totalScore=0;
 		public static void One()
 			{
 				String Snack1 = "Snack x = new Snack();";
@@ -21,12 +22,15 @@ public class Ask
 				if (userGuess.equals("A") || userGuess.equals("a") )
 						{
 					
+					userScore++;
 					System.out.println("Correct");
+					totalScore++;
 						}
+				
 				else
 					{
-					System.out.println("InCorrect");
-						
+					System.out.println("Incorrect");
+					totalScore++;	
 					}
 			}
 		public static void Two()
@@ -46,13 +50,15 @@ public class Ask
 
 				if (userGuess.equals("B") || userGuess.equals("b") )
 						{
-					
+					userScore++;
 					System.out.println("Correct");
+					totalScore++;
 						}
+				
 				else
 					{
 					System.out.println("Incorrect");
-						
+					totalScore++;	
 					}
 			}
 		public static void Three()
@@ -72,13 +78,15 @@ public class Ask
 
 				if (userGuess.equals("D") || userGuess.equals("d") )
 						{
-					
+					userScore++;
 					System.out.println("Correct");
+					totalScore++;
 						}
+				
 				else
 					{
 					System.out.println("Incorrect");
-						
+					totalScore++;
 					}
 			}
 		public static void Four()
@@ -90,21 +98,23 @@ public class Ask
 				System.out.println("If the code runs:");
 				System.out.println(Snack1);
 				System.out.println(run1);
-				System.out.println("What will the result be? A. Snacks are delicious  B. There isn't a 'best' snack  C. FRUIT IS DA BEST D. The code will not execute");
+				System.out.println("What will the result be? A. There isn't a 'best' snack  B. Snacks are delicious  C. The code will not execute D. FRUIT IS DA BEST ");
 
 				Scanner userInput2 = new Scanner (System.in);
 					
 				String userGuess = userInput2.nextLine();
 
-				if (userGuess.equals("A") || userGuess.equals("a") )
+				if (userGuess.equals("B") || userGuess.equals("b") )
 						{
-					
+					userScore++;
 					System.out.println("Correct");
+					totalScore++;
 						}
+				
 				else
 					{
-					System.out.println("InCorrect");
-						
+					System.out.println("Incorrect");
+					totalScore++;	
 					}
 			}
 		public static void Five()
@@ -116,21 +126,23 @@ public class Ask
 				System.out.println("If the code runs:");
 				System.out.println(Snack1);
 				System.out.println(run1);
-				System.out.println("What will the result be? A. Snacks are delicious  B. There isn't a 'best' snack  C. FRUIT IS DA BEST D. The code will not execute");
+				System.out.println("What will the result be? A. There isn't a 'best' snack  B. Snacks are delicious  C. The code will not execute D. FRUIT IS DA BEST ");
 
 				Scanner userInput2 = new Scanner (System.in);
 					
 				String userGuess = userInput2.nextLine();
 
-				if (userGuess.equals("C") || userGuess.equals("c") )
+				if (userGuess.equals("D") || userGuess.equals("d") )
 						{
-					
+					userScore++;
 					System.out.println("Correct");
+					totalScore++;
 						}
+				
 				else
 					{
 					System.out.println("Incorrect");
-						
+					totalScore++;	
 					}
 			}
 		public static void Six()
@@ -142,23 +154,196 @@ public class Ask
 				System.out.println("If the code runs:");
 				System.out.println(Snack1);
 				System.out.println(run1);
-				System.out.println("What will the result be? A. Snacks are delicious  B. There isn't a 'best' snack  C. FRUIT IS DA BEST D. The code will not execute");
+				System.out.println("What will the result be? A. There isn't a 'best' snack  B. Snacks are delicious  C. The code will not execute D. FRUIT IS DA BEST ");
 
 				Scanner userInput2 = new Scanner (System.in);
 					
 				String userGuess = userInput2.nextLine();
 
-				if (userGuess.equals("D") || userGuess.equals("d") )
+				if (userGuess.equals("C") || userGuess.equals("c") )
 						{
-					
+					userScore++;
 					System.out.println("Correct");
+					totalScore++;
 						}
+				
 				else
 					{
 					System.out.println("Incorrect");
-						
+					totalScore++;
 					}
 			}
-		
+		public static void Seven()
+		{
+			String Snack1 = "Fruit x = new Snack();";
+			String run1 = "x.Snacksare1()";
+			
+			
+			System.out.println("If the code runs:");
+			System.out.println(Snack1);
+			System.out.println(run1);
+			System.out.println("What will the result be? A. The code will not execute  B. FRUIT IS DA BEST  C.There isn't a 'best' snack  D. Snacks are delicious");
+
+			Scanner userInput2 = new Scanner (System.in);
+				
+			String userGuess = userInput2.nextLine();
+
+			if (userGuess.equals("A") || userGuess.equals("a") )
+					{
+				userScore++;
+				System.out.println("Correct");
+				totalScore++;
+					}
+			
+			else
+				{
+				System.out.println("Incorrect");
+				totalScore++;
+				}
+		}
+	public static void Eight()
+		{
+			String Snack1 = "Fruit x = new Snack();";
+			String run1 = "x.Snacksare2()";
+			
+			
+			System.out.println("If the code runs:");
+			System.out.println(Snack1);
+			System.out.println(run1);
+			System.out.println("What will the result be? A. The code will not execute  B. FRUIT IS DA BEST  C.There isn't a 'best' snack  D. Snacks are delicious");
+
+			Scanner userInput2 = new Scanner (System.in);
+				
+			String userGuess = userInput2.nextLine();
+
+			if (userGuess.equals("A") || userGuess.equals("a") )
+					{
+				
+				userScore++;
+				System.out.println("Correct");
+				totalScore++;
+					}
+			
+			else
+				{
+				System.out.println("Incorrect");
+				totalScore++;
+				}
+		}
+	public static void Nine()
+		{
+			String Snack1 = "Fruit x = new Snack();";
+			String run1 = "x.Fruitis1()";
+			
+			
+			System.out.println("If the code runs:");
+			System.out.println(Snack1);
+			System.out.println(run1);
+			System.out.println("What will the result be? A. The code will not execute  B. FRUIT IS DA BEST  C.There isn't a 'best' snack  D. Snacks are delicious");
+
+			Scanner userInput2 = new Scanner (System.in);
+				
+			String userGuess = userInput2.nextLine();
+
+			if (userGuess.equals("A") || userGuess.equals("a") )
+					{
+				userScore++;
+				System.out.println("Correct");
+				totalScore++;
+					}
+			
+			else
+				{
+				System.out.println("Incorrect");
+				totalScore++;
 		
 	}
+			int percentage = (userScore/totalScore);
+					System.out.println("Your score is: "+ userScore + "/" + totalScore);
+}
+	public static void Ten()
+	{
+		String Snack1 = "Fruit x = new Fruit();";
+		String run1 = "x.Snacksare1()";
+		
+		
+		System.out.println("If the code runs:");
+		System.out.println(Snack1);
+		System.out.println(run1);
+		System.out.println("What will the result be? A. There isn't a 'best' snack  B. Snacks are delicious  C. FRUIT IS DA BEST D. The code will not execute");
+
+		Scanner userInput2 = new Scanner (System.in);
+			
+		String userGuess = userInput2.nextLine();
+
+		if (userGuess.equals("B") || userGuess.equals("b") )
+				{
+			userScore++;
+			System.out.println("Correct");
+			totalScore++;
+				}
+		
+		else
+			{
+			System.out.println("Incorrect");
+			totalScore++;
+			}
+	}
+public static void Eleven()
+	{
+		String Snack1 = "Fruit x = new Fruit();";
+		String run1 = "x.Snacksare2()";
+		
+		
+		System.out.println("If the code runs:");
+		System.out.println(Snack1);
+		System.out.println(run1);
+		System.out.println("What will the result be? A. Snacks are delicious  B. The code will not execute  C. FRUIT IS DA BEST D. There isn't a 'best' snack");
+
+		Scanner userInput2 = new Scanner (System.in);
+			
+		String userGuess = userInput2.nextLine();
+
+		if (userGuess.equals("C") || userGuess.equals("c") )
+				{
+			userScore++;
+			System.out.println("Correct");
+			totalScore++;
+				}
+		
+		else
+			{
+			System.out.println("Incorrect");
+			totalScore++;
+			}
+	}
+public static void Twelve()
+	{
+		String Snack1 = "Fruit x = new Fruit();";
+		String run1 = "x.Fruitis1()";
+		
+		
+		System.out.println("If the code runs:");
+		System.out.println(Snack1);
+		System.out.println(run1);
+		System.out.println("What will the result be? A. Fruit is sweet  B. FRUIT IS DA BEST  C.  The code will not execute D. There isn't a 'best' snack");
+
+		Scanner userInput2 = new Scanner (System.in);
+			
+		String userGuess = userInput2.nextLine();
+
+		if (userGuess.equals("A") || userGuess.equals("a") )
+				{
+			
+			userScore++;
+			System.out.println("Correct");
+			totalScore++;
+				}
+		
+		else
+			{
+			System.out.println("Incorrect");
+			totalScore++;
+	}
+	}
+}
--- Gmae/src/Game.java
+++ Gmae/src/Game.java
@@ -5,7 +5,7 @@ public class Game
 
 		public static void main(String[] args)
 			{
-System.out.println("How many questions would you like to have in this short quiz? (max 8)");
+System.out.println("How many questions would you like to have in this short quiz? (max 12)");
 				
 				Scanner userInput = new Scanner (System.in);
 					
@@ -13,54 +13,125 @@ System.out.println("How many questions would you like to have in this short quiz
 			
 			if (choice == 1 )
 				{
-				Ask.One();	
+				Ask.Nine();	
 				}
 			if (choice == 2)
 				{
+				
 				Ask.One();
-				Ask.Two();
+				Ask.Nine();
 				}
 			if (choice == 3 )
 					{
 				Ask.One();
-				Ask.Two();
 				Ask.Three();
+				Ask.Nine();
+				
 					}
 			if (choice == 4)
 					{
-				Ask.One();
-				Ask.Two();	
-				Ask.Three();
-				Ask.Four();
+				Ask.Twelve();
+				Ask.Four();	
+				Ask.Two();
+				Ask.Nine();
 			
 					}
 			if (choice == 5 )
 				{
+				Ask.Eight();
 				Ask.One();
 				Ask.Two();
-				Ask.Three();
-				Ask.Four();
-				Ask.Five();
+				Ask.Twelve();
+				Ask.Nine();
 				}
 			if (choice == 6)
 				{
-				Ask.One();
-				Ask.Two();
+				Ask.Twelve();
 				Ask.Three();
+				Ask.Two();
 				Ask.Four();
 				Ask.Five();
-				Ask.Six();
+				Ask.Nine();
 				}
 			if (choice == 7 )
 					{
-						
+				Ask.Seven();
+				Ask.Three();
+				Ask.Twelve();
+				Ask.Four();
+				Ask.Five();
+				Ask.Six();
+				Ask.Nine();
 					}
 			if (choice == 8)
 					{
-						
+				Ask.Eight();
+				Ask.Two();
+				Ask.Six();
+				Ask.Twelve();
+				Ask.One();
+				Ask.Three();
+				Ask.Seven();
+				Ask.Nine();
 			
 					}
-					
+			if (choice == 9 )
+				{
+				Ask.Eleven();
+				Ask.Four();
+				Ask.Two();
+				Ask.Five();
+				Ask.Three();
+				Ask.Eight();
+				Ask.Seven();
+				Ask.Ten();
+				Ask.Nine();
+				}
+			if (choice == 10)
+				{
+				Ask.Ten();
+				Ask.Four();
+				Ask.Two();
+				Ask.Five();
+				Ask.Three();
+				Ask.Eight();
+				Ask.Seven();
+				Ask.One();
+				Ask.Six();
+				Ask.Nine();
+	
+				}
+			if (choice == 11 )
+				{
+				Ask.Ten();
+				Ask.Four();
+				Ask.Two();
+				Ask.Five();
+				Ask.Three();
+				Ask.Eight();
+				Ask.Seven();
+				Ask.Eleven();
+				Ask.Six();
+				Ask.One();
+				Ask.Nine();
+				}
+			if (choice == 12)
+				{
+				
+				Ask.Eleven();
+				Ask.Ten();
+				Ask.Four();
+				Ask.Two();
+				Ask.Five();
+				Ask.Three();
+				Ask.Eight();
+				Ask.Seven();
+				Ask.Twelve();
+				Ask.Six();
+				Ask.One();
+				Ask.Nine();
+	
+				}	
 			}
 
 	}
